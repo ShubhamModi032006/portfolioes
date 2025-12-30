1c537ce29805d3ef454aa6d601f9f84db11208bb
shubhamiscodding <shubham.modi.cg@gmail.com>
2025-12-30 15:31:53 +0530

    feat: add Resume component to display an embedded resume with navigation and download options.

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -8,25 +8,25 @@ const Resume = () => {
   const { theme } = useTheme(); // Hook for theme
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900 evening:bg-evening-background text-gray-900 dark:text-white evening:text-evening-primary p-4 sm:p-6 w-full">
+    <div className="flex flex-col items-center h-screen bg-gray-100 dark:bg-gray-900 evening:bg-evening-background text-gray-900 dark:text-white evening:text-evening-primary p-4 w-full overflow-hidden">
       {/* Back Button */}
       <button
         onClick={() => navigate("/")}
-        className="absolute top-4 sm:top-6 left-4 sm:left-6 flex items-center bg-gray-200 dark:bg-gray-800 evening:bg-evening-secondary hover:bg-gray-300 dark:hover:bg-gray-700 evening:hover:bg-evening-primary px-3 sm:px-4 py-2 rounded-lg transition duration-300 text-gray-900 dark:text-white evening:text-evening-background"
+        className="absolute top-4 left-4 sm:left-6 flex items-center bg-gray-200 dark:bg-gray-800 evening:bg-evening-secondary hover:bg-gray-300 dark:hover:bg-gray-700 evening:hover:bg-evening-primary px-3 sm:px-4 py-2 rounded-lg transition duration-300 text-gray-900 dark:text-white evening:text-evening-background z-10"
       >
         <ArrowLeft className="w-4 sm:w-5 h-4 sm:h-5 mr-2" />
         Back to Portfolio
       </button>
 
       {/* Resume Heading */}
-      <h1 className="text-3xl sm:text-4xl font-bold mb-4 sm:mb-6 text-gray-900 dark:text-white evening:text-evening-primary">My Resume</h1>
+      <h1 className="text-2xl sm:text-3xl font-bold mt-12 mb-2 text-gray-900 dark:text-white evening:text-evening-primary">My Resume</h1>
 
       {/* Resume PDF Embed */}
-      <img
-        src="https://res.cloudinary.com/dqhn4dq02/image/upload/v1740114838/in3byxp5pcwlingjsuby.jpg"
-        className="max-w-full sm:max-w-4xl h-auto sm:h-[800px] border rounded-lg shadow-lg"
+      <iframe
+        src="https://drive.google.com/file/d/1Y3xLQZPY8RYeQ_CNiWjOWVmiCN2_i7Mu/preview"
+        className="w-full max-w-4xl flex-1 border rounded-lg shadow-lg"
         title="Resume"
-      ></img>
+      ></iframe>
 
       {/* Download Button */}
       <a
@@ -34,7 +34,7 @@ const Resume = () => {
         download="https://drive.google.com/file/d/1Y3xLQZPY8RYeQ_CNiWjOWVmiCN2_i7Mu/view?usp=sharing"
         target="_blank"
         rel="noopener noreferrer"
-        className="mt-4 sm:mt-6 bg-green-600 hover:bg-green-700 text-white px-4 sm:px-6 py-2 sm:py-3 rounded-lg transition duration-300"
+        className="mt-4 bg-green-600 hover:bg-green-700 text-white px-6 py-2 rounded-lg transition duration-300 shadow-md mb-2"
       >
         Download Resume
       </a>
